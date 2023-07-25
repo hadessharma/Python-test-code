422be59edfa22ae913d06b512e0e7730ce7bb8d3
hadessharma <scsa990@gmail.com>
2023-07-25 14:55:14 +0530

    added pop method in the ll

--- ll/ll.py
+++ ll/ll.py
@@ -16,12 +16,42 @@ class LinkedList:
             print(temp.val)
             temp = temp.next
     
-    def append_list(self, val) -> None:
-        temp = self.head
+    def append_list(self, val) -> bool:
         new_node = Node(val=val)
+        
+        if  not self.head:
+            self.head    = new_node
+            self.tail    = new_node
+            self.length += 1
+            return True
+            
+        temp = self.head
+        
         while temp.next:
             temp = temp.next
-        temp.next = new_node
-        self.tail = new_node
             
+        temp.next    = new_node
+        self.tail    = new_node
+        self.length += 1
         
+        return True
+        
+    def pop(self):
+        if not self.head:
+            return None
+        
+        temp = self.head
+        
+        if not self.head.next:
+            self.length -= 1
+            self.head, self.tail = None, None
+            return temp
+
+        while temp.next.next:
+            temp = temp.next
+            
+        self.tail       = temp
+        popped          = temp.next
+        self.tail.next  = None
+        self.length    -= 1
+        return popped
